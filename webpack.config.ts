eef8c7037adbbc559d689f94c3a489961dd57593
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2022-04-23 07:36:53 +0700

    minify css

--- webpack.config.ts
+++ webpack.config.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { CleanWebpackPlugin } from "clean-webpack-plugin";
 import HtmlWebPackPlugin from "html-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import CssMinimizerPlugin from "css-minimizer-webpack-plugin";
 
 import type { Configuration as DevServerConfiguration } from "webpack-dev-server";
 import type { Configuration } from "webpack";
@@ -56,6 +57,9 @@ export default function build(env: any, arg: any): WebpackConfiguration {
           ],
         },
       ],
+    },
+    optimization: {
+      minimizer: [`...`, new CssMinimizerPlugin()],
     },
     plugins: [
       new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
